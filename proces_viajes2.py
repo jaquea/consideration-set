1ee712b0b2a25ac562fcc727845429892c9f5bae
Felipe Hernández <cephei.1313@gmail.com>
2020-09-09 10:05:01 -0300

    corrección de nombre de archivo

--- proces_viajes2.py
+++ proces_viajes2.py
@@ -18,7 +18,7 @@ dump_file2 = open('tmp\\viajes_reales.pkl', 'rb')
 viajes_reales = dill.load(dump_file2)
 dump_file2.close()
 
-dump_file1 = open('tmp\\viajes_alternativas_desaglosadas.pkl', 'rb')
+dump_file1 = open('tmp\\viajes_alternativas_desglosadas.pkl', 'rb')
 viajes_alternativas_desaglosadas = dill.load(dump_file1)
 dump_file1.close()
 
